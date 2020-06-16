e9829bebcad67ed03ab6abb7c78c410872f85ad4
xiagen <xiag@cnliren.cn>
2020-06-16 10:43:21 +0800

    配置修改

--- oauth-server/src/main/java/cn/cnyaoshun/oauth/service/impl/UserDetailServiceImpl.java
+++ oauth-server/src/main/java/cn/cnyaoshun/oauth/service/impl/UserDetailServiceImpl.java
@@ -5,6 +5,7 @@ import cn.cnyaoshun.oauth.common.exception.ExceptionValidation;
 import cn.cnyaoshun.oauth.dao.AccountRepository;
 import cn.cnyaoshun.oauth.entity.Account;
 import cn.cnyaoshun.oauth.entity.UserDetailsImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 
 @Service("UserDetailServiceImpl")
+@Slf4j
 public class UserDetailServiceImpl implements UserDetailsService {
 
     @Autowired
@@ -20,8 +22,10 @@ public class UserDetailServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        log.info("userName :"+userName);
         Account account = accountRepository.findByAccountName(userName);
         if (account == null){
+            log.info("account:");
             throw new ExceptionValidation(ApiCode.DATA_NOT_EXISTS.getCode(),"用户不存在");
         }
         return new UserDetailsImpl(account);
