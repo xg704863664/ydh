4b6d1a6800a660adca4a2e004a96a100a6624a9a
xiagen <xiag@cnliren.cn>
2020-06-19 14:02:05 +0800

    zuul修改

--- api-gateway-server/src/main/java/cn/cnyaoshun/zuul/filter/CorsFilter.java
+++ api-gateway-server/src/main/java/cn/cnyaoshun/zuul/filter/CorsFilter.java
@@ -21,7 +21,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN, content-disposition, content-length, cache-control");
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
