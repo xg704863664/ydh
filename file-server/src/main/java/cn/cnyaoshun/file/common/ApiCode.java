576753aabb5c09cddf0204becc0ef9a25a3a0c75
lidc <lidc@cnliren.cn>
2020-06-23 15:39:29 +0800

    Update the ApiCode.java

--- file-server/src/main/java/cn/cnyaoshun/file/common/ApiCode.java
+++ file-server/src/main/java/cn/cnyaoshun/file/common/ApiCode.java
@@ -3,14 +3,14 @@ package cn.cnyaoshun.file.common;
 import lombok.Getter;
 
 public enum ApiCode {
-    NOT_FOUNT_ACCESS_TOKEN(417,"token不存在"),
+    NOT_FOUNT_ACCESS_TOKEN(417,"Token不存在"),
     SUCCESS(0, "请求成功"),
     SYSTEM_BUSY(100, "系统繁忙"),
     REQUEST_TIME_OUT(300, "请求超时"),
     PARAMETER_ERROR(400, "参数错误"),
     NETWORK_ERROR(404, "网络异常"),
     DATA_NOT_EXISTS(600, "数据不存在"),
-    ACCESSDENIED_ERROR(501,"你无权访问"),
+    ACCESSDENIED_ERROR(501,"无权访问"),
     FAILURE(999, "未知错误"),
     UPLOAD_File_FAIL(418,"文件上传失败"),
     DOWNLOAD_File_FAIL(419,"文件下载失败"),
