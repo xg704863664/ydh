613d1609b5a5d8da63b220896e00775b84ae6e80
xiagen <xiag@cnliren.cn>
2020-06-28 11:17:22 +0800

    插件和打包命令修改

--- packaging.bat
+++ packaging.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
-call mvn clean package -Dmaven.test.skip=true
+call mvn clean package antrun:run  -Dmaven.test.skip=true
 call mvn assembly:single -Dmaven.test.skip=true
 rd /s/q !cd!\jar
 pause
