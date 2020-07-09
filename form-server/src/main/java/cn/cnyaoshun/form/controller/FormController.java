cc3c5c78121fd486d3eb1a6db86bdcd0272149a8
zhaoyp <zhaoyp@cnliren.cn>
2020-07-09 18:24:05 +0800

    表单开发

--- form-server/src/main/java/cn/cnyaoshun/form/controller/FormController.java
+++ form-server/src/main/java/cn/cnyaoshun/form/controller/FormController.java
@@ -6,11 +6,12 @@ import cn.cnyaoshun.form.designer.model.Designer;
 import cn.cnyaoshun.form.designer.service.DesignerService;
 import cn.cnyaoshun.form.designer.service.FormService;
 import com.alibaba.fastjson.JSON;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.constraints.Min;
@@ -20,6 +21,9 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/form")
+@Api(description = "数据源管理")
+@Validated
 public class FormController {
 
     @Resource
@@ -33,6 +37,7 @@ public class FormController {
      * @param designerId
      * @return
      */
+    @ApiOperation(value = "加载表头",httpMethod = "GET",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @GetMapping("/loadTableName/{designerId}")
     public ReturnJsonData<List<Map<String,Object>>> loadTableName(@ApiParam(value = "设计器id",required = true)@PathVariable(value = "designerId") Long designerId){
         Designer designer = designerService.findById(designerId);
@@ -54,14 +59,33 @@ public class FormController {
      * @param designerId
      * @return
      */
+    @ApiOperation(value = "加载分页数据",httpMethod = "POST",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @PostMapping("/findByPage")
     public ReturnJsonData<PageDataDomain<Map<String,Object>>> findByPage(@Min(1) @ApiParam(value = "当前页",required = true)@RequestParam(value = "pageNumber") Integer pageNumber,
                                                                                @Min(1) @ApiParam(value = "每页显示数量", required = true)@RequestParam(value = "pageSize")  Integer pageSize,
                                                                                @ApiParam(value = "设计器id", required = true)@RequestParam(value = "designerId")  Long designerId){
         return ReturnJsonData.build(formService.findByPage(pageNumber,pageSize,designerId));
     }
 
-    public ReturnJsonData<Map<String,Object>> findById(@ApiParam(value = "设计器id", required = true)@RequestParam(value = "designerId")  Long designerId,
-                                                       @ApiParam(value = "本数据id", required = true)@RequestParam(value = "id")  String id){
-        return null;
+    @ApiOperation(value = "根据id查询数据",httpMethod = "GET",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping("/findById/{designerId}/{id}")
+    public ReturnJsonData<Map<String,Object>> findById(@ApiParam(value = "设计器id", required = true)@PathVariable(value = "designerId")  Long designerId,
+                                                       @ApiParam(value = "本数据id", required = true)@PathVariable(value = "id")  String id){
+        return ReturnJsonData.build(formService.findById(id,designerId));
+    }
+
+    @ApiOperation(value = "根据id删除数据",httpMethod = "DELETE",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @DeleteMapping("/delete/{designerId}/{id}")
+    public ReturnJsonData delete(@ApiParam(value = "设计器id", required = true) @PathVariable(value = "designerId")  Long designerId,
+                                 @ApiParam(value = "本数据id", required = true) @PathVariable(value = "id")  String id){
+        formService.delete(id,designerId);
+        return ReturnJsonData.build();
+    }
+
+    @ApiOperation(value = "保存数据（修改数据时，需要传id）",httpMethod = "POST",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @PostMapping("/save")
+    public ReturnJsonData save(@ApiParam(value = "参数集合", required = true)@RequestParam Map<String,Object> map){
+        formService.save(map);
+        return ReturnJsonData.build();
     }
 }
