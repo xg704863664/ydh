27f869e5ae7fbd8d8e3e381cb54f96a5d6a236bb
zhaoyp <zhaoyp@cnliren.cn>
2020-07-09 18:53:38 +0800

    表单开发

---
+++ form-server/src/main/java/cn/cnyaoshun/form/common/domain/OauthUserListDomain.java
@@ -0,0 +1,31 @@
+package cn.cnyaoshun.form.common.domain;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @ClassName OauthUserListDomain
+ * @Description 根据项目ID,token 获取用户所有信息
+ * @Author fyh
+ * Date 2020/6/1910:15
+ */
+@Data
+public class OauthUserListDomain {
+
+    @ApiModelProperty(value = "账户ID")
+    private Long accountId;
+
+    @ApiModelProperty(value = "角色集合")
+    private List<RoleFindAllByOauthDomain> roleList;
+
+    @ApiModelProperty(value = "权限集合")
+    private List<PermissionOauthUserListDomain> permissionList;
+
+    @ApiModelProperty(value = "用户ID")
+    private Long userId;
+
+    @ApiModelProperty(value = "用户名称")
+    private String userName;
+}
---
+++ form-server/src/main/java/cn/cnyaoshun/form/common/domain/PermissionOauthUserListDomain.java
@@ -0,0 +1,24 @@
+package cn.cnyaoshun.form.common.domain;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @ClassName permissionDomain
+ * @Description 根据项目ID和TOKEN获取权限信息
+ * @Author fyh
+ * Date 2020/6/179:57
+ */
+@Data
+public class PermissionOauthUserListDomain {
+
+    @ApiModelProperty(value = "权限ID")
+    private Long id;
+
+    @ApiModelProperty(value = "权限名称非空")
+    private String permissionName;
+
+    @ApiModelProperty(value = "权限编码")
+    private String permissionType;
+
+}
