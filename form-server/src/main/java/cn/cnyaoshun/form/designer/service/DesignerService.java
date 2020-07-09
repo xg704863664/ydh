4441871eb470dbd812c33bf9a45b25362583f93c
zhaoyp <zhaoyp@cnliren.cn>
2020-07-09 14:44:02 +0800

    设计器代码

---
+++ form-server/src/main/java/cn/cnyaoshun/form/designer/service/DesignerService.java
@@ -0,0 +1,22 @@
+package cn.cnyaoshun.form.designer.service;
+
+import cn.cnyaoshun.form.common.PageDataDomain;
+import cn.cnyaoshun.form.designer.model.Designer;
+
+import java.util.List;
+
+public interface DesignerService {
+
+    Designer findById(Long id);
+
+    List<Designer> findByOrgIdAndStatus(Long orgId,boolean status);
+
+    PageDataDomain<Designer> findByPage(Integer pageNum,Integer pageSize,Long orgId);
+
+    Designer save(Designer designer);
+
+    void delete(Long id);
+
+    Designer updateStatus(Long id,boolean status);
+
+}
