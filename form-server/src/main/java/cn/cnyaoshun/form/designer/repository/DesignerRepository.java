a36fd75003f9079158303ce712ffc7b276ffba8d
zhaoyp <zhaoyp@cnliren.cn>
2020-07-15 10:12:02 +0800

    换接口

--- form-server/src/main/java/cn/cnyaoshun/form/designer/repository/DesignerRepository.java
+++ form-server/src/main/java/cn/cnyaoshun/form/designer/repository/DesignerRepository.java
@@ -1,19 +1,15 @@
 package cn.cnyaoshun.form.designer.repository;
 
 import cn.cnyaoshun.form.designer.model.Designer;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface DesignerRepository extends PagingAndSortingRepository<Designer, Long> {
+public interface DesignerRepository extends PagingAndSortingRepository<Designer, Long>, JpaSpecificationExecutor<Designer> {
 
     List<Designer> findByOrgIdAndStatus(Long orgId, boolean status);
 
-    Page<Designer> findByOrgId(Long orgId, Pageable page);
-
-    Page<Designer> findByOrgIdAndNameLike(Long orgId, String name, Pageable page);
 }
--- form-server/src/main/java/cn/cnyaoshun/form/designer/service/impl/DesignerServiceImpl.java
+++ form-server/src/main/java/cn/cnyaoshun/form/designer/service/impl/DesignerServiceImpl.java
@@ -10,14 +10,18 @@ import cn.cnyaoshun.form.designer.repository.DesignerRepository;
 import cn.cnyaoshun.form.designer.service.DesignerService;
 import cn.cnyaoshun.form.remote.OauthServerClient;
 import com.alibaba.fastjson.JSON;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -44,12 +48,21 @@ public class DesignerServiceImpl implements DesignerService {
     @Override
     public PageDataDomain<Designer> findByPage(Integer pageNum, Integer pageSize, Long orgId, String searchValue) {
         Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
-        Page<Designer> page = null;
-        if(StringUtils.isNotBlank(searchValue)){
-            page = designerRepository.findByOrgIdAndNameLike(orgId,"%"+searchValue+"%",pageable);
-        }else{
-            page = designerRepository.findByOrgId(orgId, pageable);
-        }
+        Specification<Designer> specification = new Specification<Designer>() {
+            @Override
+            public Predicate toPredicate(Root<Designer> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                List<Predicate> predicates = new ArrayList<Predicate>();
+                if (orgId != null) {
+                    predicates.add(criteriaBuilder.equal(root.get("orgId"), orgId));
+                }
+                if (searchValue != null) {
+                    predicates.add(criteriaBuilder.like(root.get("name"), "%" + searchValue + "%"));
+                }
+                criteriaQuery.where(predicates.toArray(new Predicate[predicates.size()]));
+                return null;
+            }
+        };
+        Page<Designer> page = designerRepository.findAll(specification, pageable);
         PageDataDomain<Designer> result = new PageDataDomain<Designer>();
         result.setCurrent(pageNum);
         result.setSize(pageSize);
