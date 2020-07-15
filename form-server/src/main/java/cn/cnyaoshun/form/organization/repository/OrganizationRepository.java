2a4af7076aff021a4d10f9405dd328ab9f26ab39
huangqh <huangqh@cnliren.cn>
2020-07-15 15:07:34 +0800

    添加修改接口

--- form-server/src/main/java/cn/cnyaoshun/form/organization/repository/OrganizationRepository.java
+++ form-server/src/main/java/cn/cnyaoshun/form/organization/repository/OrganizationRepository.java
@@ -1,6 +1,7 @@
 package cn.cnyaoshun.form.organization.repository;
 
 import cn.cnyaoshun.form.organization.model.Organization;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,6 +10,5 @@ import java.util.List;
  * @Author:
  * @Date: 2020/7/8 11:26
  */
-public interface OrganizationRepository extends CrudRepository<Organization,Long> {
-      List<Organization> findAll();
+public interface OrganizationRepository extends CrudRepository<Organization,Long>, JpaSpecificationExecutor<Organization> {
 }
