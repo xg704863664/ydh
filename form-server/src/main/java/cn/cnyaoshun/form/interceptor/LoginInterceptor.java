de074fc789e7b749c32442990b728ebc9d7cbc54
zhaoyp <zhaoyp@cnliren.cn>
2020-07-21 14:35:05 +0800

    提交错了，还原回去

--- form-server/src/main/java/cn/cnyaoshun/form/interceptor/LoginInterceptor.java
+++ form-server/src/main/java/cn/cnyaoshun/form/interceptor/LoginInterceptor.java
@@ -1,5 +1,11 @@
 package cn.cnyaoshun.form.interceptor;
 
+import cn.cnyaoshun.form.common.ApiCode;
+import cn.cnyaoshun.form.common.AppContextAware;
+import cn.cnyaoshun.form.common.ReturnJsonData;
+import cn.cnyaoshun.form.common.exception.ExceptionAuth;
+import cn.cnyaoshun.form.remote.OauthServerClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -11,16 +17,16 @@ public class LoginInterceptor implements HandlerInterceptor  {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-//        String token = request.getHeader("Authorization");
-//        if (StringUtils.isEmpty(token)){
-//            throw new ExceptionAuth(ApiCode.NOT_FOUNT_ACCESS_TOKEN.getCode(), "token is not null");
-//        }
-//        OauthServerClient oauthServerClient = AppContextAware.getApplicationContext().getBean(OauthServerClient.class);
-//        ReturnJsonData<String> returnJsonData = oauthServerClient.checkToken(token);
-//        if (returnJsonData.getCode() == 0) {
-//            return true;
-//        }
-//        throw new ExceptionAuth(returnJsonData.getCode(), returnJsonData.getMsg());
-        return true;
+        String token = request.getHeader("Authorization");
+        if (StringUtils.isEmpty(token)){
+            throw new ExceptionAuth(ApiCode.NOT_FOUNT_ACCESS_TOKEN.getCode(), "token is not null");
+        }
+        OauthServerClient oauthServerClient = AppContextAware.getApplicationContext().getBean(OauthServerClient.class);
+        ReturnJsonData<String> returnJsonData = oauthServerClient.checkToken(token);
+        if (returnJsonData.getCode() == 0) {
+            return true;
+        }
+        throw new ExceptionAuth(returnJsonData.getCode(), returnJsonData.getMsg());
+//        return true;
     }
 }
